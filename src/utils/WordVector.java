e431e676d01cc2f271e306fae297d9c0e622e4ce
Yuan Zhang <yuanzh@csail.mit.edu>
2015-05-03 20:31:35 -0400

    lexical

--- src/utils/WordVector.java
+++ src/utils/WordVector.java
@@ -20,12 +20,43 @@ public class WordVector {
 		loadWordVector();
 	}
 	
+	public String constructFileName(int l) {
+		return options.dataDir + "/universal_treebanks_v2.0/" + langString[l] + ".vec.100";
+	}
+	
 	public void loadWordVector() throws IOException {
+		System.out.print("load word vectors: ");
 		wordVec = new double[langNum][][];
 		dicts = new TObjectIntHashMap[langNum];
 		
 		for (int lang = 0; lang < langNum; ++lang) {
-			
+			System.out.print(" " + langString[lang] + " ");
+			dicts[lang] = new TObjectIntHashMap<String>();
+			BufferedReader br = new BufferedReader(new FileReader(constructFileName(lang)));
+			String[] data = br.readLine().split("\\s+");
+			int wordNum = Integer.parseInt(data[0]);
+			int dim = Integer.parseInt(data[1]);
+			wordVec[lang] = new double[wordNum][dim];
+			for (int i = 0; i < wordNum; ++i) {
+				data = br.readLine().split("\\s+");
+				dicts[lang].put(data[0], i + 1);
+				for (int l = 0; l < dim; ++l) {
+					wordVec[lang][i][l] = Double.parseDouble(data[l + 1]);
+				}
+			}
+			br.close();
 		}
+		System.out.println("Done.");
+	}
+	
+	public int getWordId(int lang, String w) {
+		if (!dicts[lang].containsKey(w))
+			return -1;
+		else
+			return dicts[lang].get(w) - 1;
+	}
+	
+	public double[] getWordVec(int lang, int id) {
+		return wordVec[lang][id];
 	}
 }
